d182e37b975b9d08e3464da89e3b11b8c2fbf705
Flo Lauber <flo@qatfy.at>
2019-05-14 19:07:58 +0200

    configure firebase from process.env

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,19 @@ import 'firebase/firestore';
 import 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: ""
+  projectId: FIREBASE_PROJECT_ID,
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
 };
+
+// check if all values are provided
+if (!Object.values(firebaseConfig).every(x => x)) {
+  throw new Error(`Firebase config incomplete; environment vars missing for: \
+    ${!FIREBASE_PROJECT_ID ? '\n   FIREBASE_PROJECT_ID' : ''} \
+    ${!FIREBASE_API_KEY ? '\n   FIREBASE_API_KEY' : ''} \
+    ${!FIREBASE_AUTH_DOMAIN ? '\n   FIREBASE_AUTH_DOMAIN' : ''}. \
+    \nYou can find these values in your firebase console at https://console.firebase.google.com/`);
+}
 
 firebase.initializeApp(firebaseConfig);
 export default firebase;
--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,9 @@ const plugins = [
     __PROD__: JSON.stringify(nodeEnv === 'production'),
     CONFIG: JSON.stringify(config),
     BASE_URL: JSON.stringify(`${process.env.BASE_URL || ''}`),
+    FIREBASE_API_KEY: JSON.stringify(`${process.env.FIREBASE_API_KEY || ''}`),
+    FIREBASE_AUTH_DOMAIN: JSON.stringify(`${process.env.FIREBASE_AUTH_DOMAIN || ''}`),
+    FIREBASE_PROJECT_ID: JSON.stringify(`${process.env.FIREBASE_PROJECT_ID || ''}`),
   }),
   new HtmlWebpackPlugin({
     filename: 'index.html',
